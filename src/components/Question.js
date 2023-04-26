74a75f43ca69ad50916df77289411ed8c29e8a76
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-26 15:31:46 +0300

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+///added a useEffect to the hook
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+useEffect(() =>{
+  const period = setTimeout(() => {
+    //decreasee amt of time
+    setTimeRemaining((prevPeriod) => prevPeriod -1)
+
+  }, 1000);
+  //cleanup function
+  return () => clearTimeout(period);
+  
+
+}, [timeRemaining]);
+//a useEffect to render when time hits 0
+useEffect(() => {
+  //if the timeremaining is 0
+  if(timeRemaining === 0){
+    //refresh time remaining
+    setTimeRemaining(10);
+    //call the onAnswered with a value false
+    onAnswered(false);
+
+  }
+})
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
